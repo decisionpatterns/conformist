0ce7cde77206d01c0a8d2235baec27ecd051d807
Christopher Brown <cbrown@decisionpatterns.com>
2015-03-13 09:38:19 -0700

    Improve .load_abberviations

--- data-raw/abbreviations.r
+++ data-raw/abbreviations.r
@@ -2,13 +2,25 @@
 
 #' load abbreviations 
 #' 
+#' @examples
+#'   system.file( "abbreviations.txt", package="conformist") %>%
+#'   .load_abbreviations
+#'   
+#'   "data-raw/abbreviations.txt"  %>% .load_abbreviations -> x
+#'  
+#'  # TO 
+#'   abbreviations <-
+#'     "data-raw/abbreviations.txt"  %>% 
+#'     .load_abbreviations 
+#'     save( abbreviations, file="data/abbreviations.rda" )
+#'     
 #' @rdname internal
 
-.load_abbreviations <- function() { 
+.load_abbreviations <- function(x) { 
 
  #  message(   system.file( "abbreviations.txt", package="conformist") )
   abbr_table = read.table( 
-    system.file( "abbreviations.txt", package="conformist")
+      x    
     , sep="\t"
     , header=TRUE
     , as.is=TRUE 
